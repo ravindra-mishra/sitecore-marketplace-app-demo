c2539c9b472911587342384abfc9fa77a47eeaeb
Ravindra Mishra <ravindra.mishra@altudo.co>
2026-03-17 14:41:29 +0530

    updating code after scaffolding

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    const allowedParentDomains = [
+      "https://marketplace-app.sitecorecloud.io",
+      "https://pages.sitecorecloud.io",
+      "https://xmapps.sitecorecloud.io",
+    ];
+
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `frame-ancestors 'self' ${allowedParentDomains.join(" ")}`,
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
